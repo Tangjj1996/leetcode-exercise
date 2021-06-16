81328d2fe68385a912f96483c25a25fe7ad29728
Tangjj1996 <tangjj_1996@163.com>
2021-06-16 23:31:46 +0800

    feat: 双指针解法

--- build/series02-most-contains.js
+++ build/series02-most-contains.js
@@ -1,17 +1,32 @@
 "use strict";
+// 1. 暴力法 o(n^2)
+// function maxArea(height: number[]): number {
+//   const len = height.length
+//   let maxArea = 0
+//   for (let i = 0; i < len; i++) {
+//     for (let j = i+1; j < len; j++) {
+//       let min = height[i]
+//       height[j] < min && (min = height[j])
+//       maxArea < min * (j - i) && (maxArea = min * (j - i))
+//     }
+//   }
+//   return maxArea
+// }
 function maxArea(height) {
-    var len = height.length;
-    var maxArea = 0;
-    for (var i = 0; i < len; i++) {
-        for (var j = i + 1; j < len; j++) {
-            var min = height[i];
-            height[j] < min && (min = height[j]);
-            maxArea < min * (j - i) && (maxArea = min * (j - i));
+    var i = 0, j = height.length - 1, res = 0;
+    while (i < j) {
+        var hmin = Math.min(height[i], height[j]);
+        res = Math.max(res, hmin * (j - i));
+        if (height[i] < height[j]) {
+            ++i;
+        }
+        else {
+            --j;
         }
     }
-    return maxArea;
+    return res;
 }
-var lits = [4, 3, 2, 1, 4];
+var lits = [1, 8, 6, 2, 5, 4, 8, 3, 7];
 var res = maxArea(lits);
 console.log(res);
 //# sourceMappingURL=series02-most-contains.js.map
--- src/series02-most-contains.ts
+++ src/series02-most-contains.ts
@@ -1,16 +1,35 @@
+// 1. 暴力法 o(n^2)
+// function maxArea(height: number[]): number {
+//   const len = height.length
+//   let maxArea = 0
+//   for (let i = 0; i < len; i++) {
+//     for (let j = i+1; j < len; j++) {
+//       let min = height[i]
+//       height[j] < min && (min = height[j])
+//       maxArea < min * (j - i) && (maxArea = min * (j - i))
+//     }
+//   }
+//   return maxArea
+// }
+
 function maxArea(height: number[]): number {
-  const len = height.length
-  let maxArea = 0
-  for (let i = 0; i < len; i++) {
-    for (let j = i+1; j < len; j++) {
-      let min = height[i]
-      height[j] < min && (min = height[j])
-      maxArea < min * (j - i) && (maxArea = min * (j - i))
+  let i = 0, j = height.length - 1, res = 0
+
+  while (i < j) {
+    const hmin = Math.min(height[i], height[j])
+    res = Math.max(res, hmin * (j - i))
+    if (height[i] < height[j]) {
+      ++i
+    } else {
+      --j
     }
   }
-  return maxArea
+
+  return res
 }
-const lits = [4,3,2,1,4]
+
+
+const lits = [1,8,6,2,5,4,8,3,7]
 const res = maxArea(lits)
 
 console.log(res)
