da09054c07f9410a061b9ada361276c5de334bd7
Tangjj1996 <tangjj_1996@163.com>
2021-06-17 00:18:12 +0800

    feat: roman algorithm

---
+++ src/series03-roman.ts
@@ -0,0 +1,12 @@
+function intToRoman(num: number): string {
+  const romanMap = new Map([
+    [0, 'I'],
+    [5, 'V'],
+    [10, 'X'],
+    [50, 'L'],
+    [100, 'C'],
+    [500, 'D'],
+    [1000, 'M']
+  ])
+  return ''
+};
