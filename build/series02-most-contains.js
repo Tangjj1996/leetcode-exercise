9d2808c4641484dd8fefa9b16c7b978707c83305
Tangjj1996 <tangjj_1996@163.com>
2021-06-16 23:13:18 +0800

    feat: find max area

--- build/series02-most-contains.js
+++ build/series02-most-contains.js
@@ -1,3 +1,17 @@
 "use strict";
-console.log('this is a test.');
+function maxArea(height) {
+    var len = height.length;
+    var maxArea = 0;
+    for (var i = 0; i < len; i++) {
+        for (var j = i + 1; j < len; j++) {
+            var min = height[i];
+            height[j] < min && (min = height[j]);
+            maxArea < min * (j - i) && (maxArea = min * (j - i));
+        }
+    }
+    return maxArea;
+}
+var lits = [4, 3, 2, 1, 4];
+var res = maxArea(lits);
+console.log(res);
 //# sourceMappingURL=series02-most-contains.js.map
--- src/series02-most-contains.ts
+++ src/series02-most-contains.ts
@@ -1,1 +1,16 @@
-console.log('this is a test.')
+function maxArea(height: number[]): number {
+  const len = height.length
+  let maxArea = 0
+  for (let i = 0; i < len; i++) {
+    for (let j = i+1; j < len; j++) {
+      let min = height[i]
+      height[j] < min && (min = height[j])
+      maxArea < min * (j - i) && (maxArea = min * (j - i))
+    }
+  }
+  return maxArea
+}
+const lits = [4,3,2,1,4]
+const res = maxArea(lits)
+
+console.log(res)
