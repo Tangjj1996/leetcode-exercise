2f537a7459ca67b18da0d52f1b23ae544edaa75b
Tangjj1996 <tangjj_1996@163.com>
2021-06-17 23:49:25 +0800

    feat: add roman algorithm

--- build/series03-roman.js
+++ build/series03-roman.js
@@ -1,3 +1,41 @@
 "use strict";
-console.log(222);
+function intToRoman(num) {
+    var romanMap = new Map([
+        [0, 'I'],
+        [5, 'V'],
+        [10, 'X'],
+        [50, 'L'],
+        [100, 'C'],
+        [500, 'D'],
+        [1000, 'M']
+    ]);
+    var res = '';
+    var len = String(num).length;
+    while (len--) {
+        // 一位数
+        if (len === 0) {
+            if (num < 4) {
+                while (num--) {
+                    res += romanMap.get(0);
+                }
+            }
+            else if (num === 4) {
+                res = romanMap.get(0) + romanMap.get(5);
+            }
+            else if (num < 9) {
+                res = romanMap.get(5);
+                while (num-- > 5) {
+                    res += romanMap.get(0);
+                }
+            }
+            else {
+                res = romanMap.get(0) + romanMap.get(10);
+            }
+        }
+    }
+    return res;
+}
+;
+var result = intToRoman(8);
+console.log(result);
 //# sourceMappingURL=series03-roman.js.map
--- src/series03-roman.ts
+++ src/series03-roman.ts
@@ -1,12 +1,46 @@
 function intToRoman(num: number): string {
-  const romanMap = new Map([
-    [0, 'I'],
-    [5, 'V'],
-    [10, 'X'],
-    [50, 'L'],
-    [100, 'C'],
-    [500, 'D'],
-    [1000, 'M']
-  ])
-  return ''
-};
+  const roman = [
+    {
+      head: 'I',
+      next: 'V',
+    },
+    {
+      head: 'X',
+      next: 'L',
+    },
+    {
+      head: 'C',
+      next: 'D',
+    },
+    {
+      head: 'M',
+    },
+  ]
+  let res = ''
+
+  let len = String(num).length
+  const totalLen = len
+  while (len--) {
+    let targetNum = Number(String(num)[totalLen - len - 1])
+
+    if (targetNum < 4) {
+      while (targetNum--) {
+        res += roman[len].head
+      }
+    } else if (targetNum === 4) {
+      res = roman[len].head + roman[len].next
+    } else if (targetNum < 9) {
+      res = roman[len].next!
+      while (targetNum-- > 5) {
+        res += roman[len].head
+      }
+    } else {
+      res = roman[len].head + roman[len + 1].head
+    }
+  }
+  return res
+}
+
+const result = intToRoman(8)
+
+console.log(result)
