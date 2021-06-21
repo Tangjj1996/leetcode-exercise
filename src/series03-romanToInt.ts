d41e7a5f480a5f4257b5fb926170693d40ca65d9
Tangjj1996 <tangjj_1996@163.com>
2021-06-21 10:16:03 +0800

    feat: new algorithm

---
+++ src/series03-romanToInt.ts
@@ -0,0 +1,46 @@
+function intToRoman(num: number): string {
+  const roman = [
+    {
+      head: 'I',
+      next: 'V',
+    },
+    {
+      head: 'X',
+      next: 'L',
+    },
+    {
+      head: 'C',
+      next: 'D',
+    },
+    {
+      head: 'M',
+    },
+  ]
+  let res = ''
+
+  let len = String(num).length
+  const totalLen = len
+  while (len--) {
+    let targetNum = Number(String(num)[totalLen - len - 1])
+
+    if (targetNum < 4) {
+      while (targetNum--) {
+        res += roman[len].head
+      }
+    } else if (targetNum === 4) {
+      res += roman[len].head + roman[len].next
+    } else if (targetNum < 9) {
+      res += roman[len].next!
+      while (targetNum-- > 5) {
+        res += roman[len].head
+      }
+    } else {
+      res += roman[len].head + roman[len + 1].head
+    }
+  }
+  return res
+}
+
+const result = intToRoman(58)
+
+console.log(result)
