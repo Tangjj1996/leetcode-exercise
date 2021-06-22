e9d68b82c6bb8c25cd63c94825a43e38dfe57e77
Tangjj1996 <tangjj_1996@163.com>
2021-06-22 22:53:17 +0800

    feat: three sum & sort & double point

--- build/series06-threeSum.js
+++ build/series06-threeSum.js
@@ -1,50 +1,88 @@
 "use strict";
+// 暴力穷举
+// export function threeSum(nums: number[]): number[][] {
+//   const len = nums.length
+//   const resArr: number[][] = []
+//   for (let i = 0; i < len; i++) {
+//     for (let j = i + 1; j < len; j++) {
+//       for (let k = j + 1; k < len; k++) {
+//         if (
+//           nums[i] + nums[j] + nums[k] === 0 &&
+//           isNotDuplication(resArr, nums[i], nums[j], nums[k])
+//         ) {
+//           resArr.push([nums[i], nums[j], nums[k]])
+//         }
+//       }
+//     }
+//   }
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isNotDuplication = exports.threeSum = void 0;
+exports.threeSum = void 0;
+//   return resArr
+// }
+// export function isNotDuplication(resArr: number[][], ...parmas: number[]) {
+//   let len = resArr.length
+//   const jLen = resArr.length
+//   // 去重
+//   while (len--) {
+//     const arr = resArr[jLen - len - 1]
+//     let aLen = 3
+//     const flagIndex: number[] = []
+//     while (aLen--) {
+//       let bLen = 3
+//       while (bLen-- > 0) {
+//         if (arr[aLen] === parmas[bLen] && !flagIndex.includes(bLen)) {
+//           flagIndex.push(bLen)
+//           if (flagIndex.length === 3) {
+//             return false
+//           }
+//           break
+//         }
+//       }
+//     }
+//   }
+//   return true
+// }
 function threeSum(nums) {
-    var len = nums.length;
+    var n = nums.length;
     var resArr = [];
-    for (var i = 0; i < len; i++) {
-        for (var j = i + 1; j < len; j++) {
-            for (var k = j + 1; k < len; k++) {
-                if (nums[i] + nums[j] + nums[k] === 0 &&
-                    isNotDuplication(resArr, nums[i], nums[j], nums[k])) {
-                    resArr.push([nums[i], nums[j], nums[k]]);
+    if (!nums || nums.length < 3) {
+        return [];
+    }
+    nums.sort(function (a, b) {
+        return a - b;
+    });
+    for (var i = 0; i < n; i++) {
+        if (nums[i] > 0) {
+            return resArr;
+        }
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+        var left = i + 1;
+        var right = n - 1;
+        while (left < right) {
+            if (nums[i] + nums[left] + nums[right] === 0) {
+                resArr.push([nums[i], nums[left], nums[right]]);
+                while (left < right && nums[left] === nums[left + 1]) {
+                    left += 1;
                 }
+                while (left < right && nums[right] === nums[right - 1]) {
+                    right -= 1;
+                }
+                left += 1;
+                right -= 1;
+            }
+            else if (nums[i] + nums[left] + nums[right] < 0) {
+                left += 1;
+            }
+            else if (nums[i] + nums[left] + nums[right] > 0) {
+                right -= 1;
             }
         }
     }
     return resArr;
 }
 exports.threeSum = threeSum;
-function isNotDuplication(resArr) {
-    var parmas = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        parmas[_i - 1] = arguments[_i];
-    }
-    var len = resArr.length;
-    var jLen = resArr.length;
-    // 去重
-    while (len--) {
-        var arr = resArr[jLen - len - 1];
-        var aLen = 3;
-        var flagIndex = [];
-        while (aLen--) {
-            var bLen = aLen + 1;
-            while (bLen-- > 0) {
-                if (arr[aLen] === parmas[bLen] && !flagIndex.includes(bLen)) {
-                    flagIndex.push(bLen);
-                    break;
-                }
-                if (flagIndex.length === 3) {
-                    return false;
-                }
-            }
-        }
-    }
-    return true;
-}
-exports.isNotDuplication = isNotDuplication;
 var nums = [-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0];
 var res = threeSum(nums);
 console.log(res);
--- src/series06-threeSum.ts
+++ src/series06-threeSum.ts
@@ -1,48 +1,96 @@
 // 暴力穷举
+// export function threeSum(nums: number[]): number[][] {
+//   const len = nums.length
+//   const resArr: number[][] = []
+//   for (let i = 0; i < len; i++) {
+//     for (let j = i + 1; j < len; j++) {
+//       for (let k = j + 1; k < len; k++) {
+//         if (
+//           nums[i] + nums[j] + nums[k] === 0 &&
+//           isNotDuplication(resArr, nums[i], nums[j], nums[k])
+//         ) {
+//           resArr.push([nums[i], nums[j], nums[k]])
+//         }
+//       }
+//     }
+//   }
+
+//   return resArr
+// }
+
+// export function isNotDuplication(resArr: number[][], ...parmas: number[]) {
+//   let len = resArr.length
+//   const jLen = resArr.length
+
+//   // 去重
+//   while (len--) {
+//     const arr = resArr[jLen - len - 1]
+//     let aLen = 3
+//     const flagIndex: number[] = []
+
+//     while (aLen--) {
+//       let bLen = 3
+
+//       while (bLen-- > 0) {
+//         if (arr[aLen] === parmas[bLen] && !flagIndex.includes(bLen)) {
+//           flagIndex.push(bLen)
+//           if (flagIndex.length === 3) {
+//             return false
+//           }
+//           break
+//         }
+//       }
+//     }
+//   }
+//   return true
+// }
+
 export function threeSum(nums: number[]): number[][] {
-  const len = nums.length
+  const n = nums.length
   const resArr: number[][] = []
-  for (let i = 0; i < len; i++) {
-    for (let j = i + 1; j < len; j++) {
-      for (let k = j + 1; k < len; k++) {
-        if (
-          nums[i] + nums[j] + nums[k] === 0 &&
-          isNotDuplication(resArr, nums[i], nums[j], nums[k])
-        ) {
-          resArr.push([nums[i], nums[j], nums[k]])
+
+  if (!nums || nums.length < 3) {
+    return []
+  }
+  nums.sort((a, b) => {
+    return a - b
+  })
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] > 0) {
+      return resArr
+    }
+
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue
+    }
+
+    let left = i + 1
+    let right = n - 1
+
+    while (left < right) {
+      if (nums[i] + nums[left] + nums[right] === 0) {
+        resArr.push([nums[i], nums[left], nums[right]])
+
+        while (left < right && nums[left] === nums[left + 1]) {
+          left += 1
         }
+
+        while (left < right && nums[right] === nums[right - 1]) {
+          right -= 1
+        }
+
+        left += 1
+        right -= 1
+      } else if (nums[i] + nums[left] + nums[right] < 0) {
+        left += 1
+      } else if (nums[i] + nums[left] + nums[right] > 0) {
+        right -= 1
       }
     }
   }
 
   return resArr
-}
-
-export function isNotDuplication(resArr: number[][], ...parmas: number[]) {
-  let len = resArr.length
-  const jLen = resArr.length
-
-  // 去重
-  while (len--) {
-    const arr = resArr[jLen - len - 1]
-    let aLen = 3
-    const flagIndex: number[] = []
-
-    while (aLen--) {
-      let bLen = 3
-
-      while (bLen-- > 0) {
-        if (arr[aLen] === parmas[bLen] && !flagIndex.includes(bLen)) {
-          flagIndex.push(bLen)
-          if (flagIndex.length === 3) {
-            return false
-          }
-          break
-        }
-      }
-    }
-  }
-  return true
 }
 
 const nums = [-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0]
