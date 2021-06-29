752081f8883077f09c4846458702961ff54d917e
Tangjj1996 <tangjj_1996@163.com>
2021-06-30 00:15:49 +0800

    fix: all listNode algorithm

---
+++ build/series15-swapPairs.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.swapPairs = void 0;
+var ListNode = /** @class */ (function () {
+    function ListNode(val, next) {
+        this.val = val;
+        this.next = next;
+    }
+    return ListNode;
+}());
+function swapPairs(head) {
+    connect(head);
+    return head;
+}
+exports.swapPairs = swapPairs;
+function connect(node) {
+    if (!node) {
+        return null;
+    }
+    var left = node;
+    var mid = node.next;
+    var right = mid === null || mid === void 0 ? void 0 : mid.next;
+    if (!mid) {
+        return left;
+    }
+    mid.next = left;
+    left = connect(right);
+    return mid;
+}
+var head = {
+    val: 1,
+    next: {
+        val: 2,
+        next: {
+            val: 3,
+            next: null,
+        },
+    },
+};
+var res = swapPairs(head);
+console.log(res);
+//# sourceMappingURL=series15-swapPairs.js.map
