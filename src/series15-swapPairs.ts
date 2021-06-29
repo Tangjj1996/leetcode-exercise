060db5df2b9ebefd322e06b3f0ea8e0a3e1398b3
Tangjj1996 <tangjj_1996@163.com>
2021-06-29 23:54:43 +0800

    feaet: add swap pairs algorithm

---
+++ src/series15-swapPairs.ts
@@ -0,0 +1,44 @@
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val: number, next: ListNode | null) {
+    this.val = val
+    this.next = next
+  }
+}
+
+export function swapPairs(head: ListNode | null): ListNode | null {
+  return connect(head)
+}
+
+function connect(node: ListNode | null) {
+  if (node === null) {
+    return null
+  }
+
+  let left = node
+  let mid = node.next
+  let right = mid?.next
+
+  if (mid === null) {
+    return left
+  }
+  mid!.next = left
+  left = connect(right!)!
+
+  return mid
+}
+
+const head = {
+  val: 1,
+  next: {
+    val: 2,
+    next: {
+      val: 3,
+      next: null,
+    },
+  },
+}
+
+const res = swapPairs(head)
+console.log(head)
